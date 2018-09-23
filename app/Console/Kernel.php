9db24dbac035f3f06b8e9423c184dc3c34a8401d
Josue Coreas <jcoreas@red4g.net>
2018-09-23 07:47:06 -0600

    changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,6 @@ class Kernel extends ConsoleKernel
       //$schedule->command('inspire')
       //->hourly();
 
-      $schedule->command('fortisiem:getLogsFortisiem')->cron('*/5 * * * *');
+      //$schedule->command('fortisiem:getLogsFortisiem')->cron('*/5 * * * *');
    }
 }
