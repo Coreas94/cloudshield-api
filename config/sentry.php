5ed5ed8f7d8a2c48720f0254d286bd12a3ef7d44
Josue Coreas <jcoreas@red4g.net>
2019-02-12 11:48:00 -0600

    changes in sentra dns

--- config/sentry.php
+++ config/sentry.php
@@ -2,7 +2,7 @@
 
 return array(
     //'dsn' => env('SENTRY_DSN'),
-    'dsn' => 'https://1217784893374aa694b048762b1affd4@sentry.io/1381556',
+    'dsn' => 'http://d5676e233af441cd907209e2ebc40f37:4f2a464c101d4ac982164ff1609700e4@172.16.20.234:9000/6',
 
     // capture release as git sha
     // 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
